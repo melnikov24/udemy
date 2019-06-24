820b2707cc4274947facc60255ce261d1e6da0a5
melnikov24 <thegalogamer@yandex.ru>
2019-06-24 23:41:30 +0300

    second2

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +
+prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
 let appData = {
-    budget : money,
-    timeData : time,
-    expenses :{money : time},
-    optionalExpenses: {},
-    income : [],
-    savings : false,
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+
+
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?", '');
+    
+    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+    appData.expenses[a] = b;
+    }
+    else {}
+};
+
+appData.moneyPerDay = appData.budget / 30
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay <100) {
+    console.log('минимальный уровень достатка')
 }
-alert("ваш бюджет на 1 день : " + appData.budget / 30);
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('средний уровень достатка');
+}
+else if (appData.moneyPerDay > 2000) {
+    console.log('высокий уровень достатка');
+}
+else {
+    console.log('error');
+}
